25147bbbb0a895eb4157d775901ee011dd2b9d08
Darcy Lizbeth Yandi Flor <yandilflorlizbeth@gmail.com>
2024-04-09 19:41:18 -0600

    Ejercicio Herencia Completo

---
+++ src/herenciaejemplo/Empleado.java
@@ -0,0 +1,19 @@
+
+package herenciaejemplo;
+
+
+public class Empleado  extends Persona{
+  
+   private double sueldo;
+
+    public Empleado(double sueldo, String nombre, String apellido, String documento) {
+        super(nombre, apellido, documento);
+        this.sueldo = sueldo;
+    }
+    
+   public String mostrarEmpleado() {
+       return getNombre()+"Apellido /n"+getApellido()+ "Documento /n"+getDocumento()+"Sueldo"+ sueldo;
+   
+
+     } 
+}
